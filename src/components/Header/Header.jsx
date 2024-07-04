7e7d49eeae26a8ab8de77ce91174ffb379457573
OlhaFedorets <fedoretsolha@gmail.com>
2024-07-04 12:00:42 +0300

    Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <img src='https://styleall.ru/wp-content/uploads/2023/02/4eebee67599783.y3jvccwymzyxlde4ndcsntcyldmxnq.jpg' />
+        </header>
+    );
+}
+
+export default Header;
