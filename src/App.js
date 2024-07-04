d52e8e75dab12ab9e5d5d27bb198e56f79dbb64f
OlhaFedorets <fedoretsolha@gmail.com>
2024-07-04 11:57:39 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-rapper-content'>
+          <Routes>
+            <Route path='/dialogs' Component={Dialogs} />
+            <Route path='/profile' Component={Profile} />
+            <Route path='/news' Component={News} />
+            <Route path='/news' Component={Music} />
+            <Route path='/news' Component={Settings} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
